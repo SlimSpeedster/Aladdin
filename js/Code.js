7382a08442a274f4d2932ff119b038aa0da20792
SlimSpeedster <yashdgandhi@outlook.com>
2021-05-26 10:30:44 -0400

    finished

--- js/Code.js
+++ js/Code.js
@@ -1,6 +1,6 @@
 const accessCode1 = "VARIABLE";
-const accessCode2 = "FUNCTION";
-const accessCode3 = "DATABASE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
 
 function clues() {
     
@@ -10,6 +10,16 @@ function clues() {
     fill("lightblue")
     text("Hint: Always changing, not constant !!", 100,70)
 // add code for displaying rest of the names and the hints.
-    
+    fill("white")
+    textSize(15)
+    text("E T A D A S B A", 700,150)
+    fill("lightblue")
+    text("Hint: Stores all information!!", 700,175)
+
+    fill("white")
+    textSize(15)
+    text("T N U F I C O N", 100,370)
+    fill("lightblue")
+    text("Hint: Performs a specific task!!", 100,395)
 
 }
--- js/System.js
+++ js/System.js
@@ -7,7 +7,11 @@ class System{
         fill("black");
         text(code,300,300)
         //add code to return true if correct answer entered.
-
+            if(actualCode === enteredCode.toUpperCase())
+                return true
+            else   
+                return false
+            
         
     }
 
